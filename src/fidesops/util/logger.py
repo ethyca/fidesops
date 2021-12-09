1625ed191b07bcd7b1e6788869043d5f2c8c9497
Sean Preston <sean@ethyca.com>
2021-12-09 08:47:17 -0500

    remove circular dependency

--- src/fidesops/util/logger.py
+++ src/fidesops/util/logger.py
@@ -3,8 +3,6 @@ import os
 from typing import Any, Mapping, Union
 from numbers import Number
 
-from fidesops.core.config import config
-
 MASKED = "MASKED"
 
 
@@ -51,7 +49,7 @@ def _can_log_pii() -> bool:
     to output any PII to the logs. Right now this is being allowed
     in test mode only.
     """
-    return config.is_test_mode
+    return os.getenv("TESTING") == "True"
 
 
 def _mask_pii_for_logs(parameter: Any) -> Any:
