1dd60ff3716164f11d8aa24de255bc79c31f37aa
Sean Preston <sean@ethyca.com>
2022-03-03 12:20:14 -0700

    pass through schema requested_at

--- src/fidesops/api/v1/endpoints/privacy_request_endpoints.py
+++ src/fidesops/api/v1/endpoints/privacy_request_endpoints.py
@@ -98,9 +98,6 @@ def create_privacy_request(
     *,
     cache: FidesopsRedis = Depends(deps.get_cache),
     db: Session = Depends(deps.get_db),
-    # client: Optional[ClientDetail] = Security(
-    #     verify_oauth_client, scopes=[scopes.PRIVACY_REQUEST_CREATE]
-    # ),
     data: conlist(PrivacyRequestCreate, max_items=50) = Body(...),  # type: ignore
 ) -> BulkPostPrivacyRequests:
     """
@@ -147,15 +144,10 @@ def create_privacy_request(
             failed.append(failure)
             continue
 
-        requested_at = privacy_request_data.requested_at
-        if requested_at is None:
-            requested_at = datetime.utcnow()
-
         kwargs = {
-            "requested_at": requested_at,
+            "requested_at": privacy_request_data.requested_at,
             "policy_id": policy.id,
             "status": "pending",
-            # "client_id": client.id,
         }
         for field in optional_fields:
             attr = getattr(privacy_request_data, field)
