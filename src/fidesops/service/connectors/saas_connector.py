012bc25669d2783b41f7304b2ad158138ce76d00
eastandwestwind <eastandwestwind@gmail.com>
2022-03-18 14:27:44 -0400

    format

--- src/fidesops/service/connectors/saas_connector.py
+++ src/fidesops/service/connectors/saas_connector.py
@@ -111,10 +111,7 @@ class SaaSConnector(BaseConnector[AuthenticatedClient]):
         """Generates and executes a test connection based on the SaaS config"""
         test_request_path = self.saas_config.test_request.path
         prepared_request: SaaSRequestParams = SaaSRequestParams(
-            method="GET",
-            path=test_request_path,
-            param={},
-            body_values={}
+            method="GET", path=test_request_path, param={}, body_values={}
         )
         self.client().send(prepared_request)
         return ConnectionTestStatus.succeeded
