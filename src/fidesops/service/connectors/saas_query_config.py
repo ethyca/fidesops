e66898bb39d05d0ecd2cefd01c0d04c463c6ef4c
eastandwestwind <eastandwestwind@gmail.com>
2022-03-18 11:16:22 -0400

    extracts out method to build request body

--- src/fidesops/service/connectors/saas_query_config.py
+++ src/fidesops/service/connectors/saas_query_config.py
@@ -68,7 +68,7 @@ class SaaSQueryConfig(QueryConfig[SaaSRequestParams]):
 
         path: str = current_request.path
         query_params: Dict[str, Any] = {}
-        body: str = current_request.body or None
+        body: Optional[str] = current_request.body or None
 
         # uses the param names to read from the input data
         for param in current_request.request_params:
@@ -80,15 +80,14 @@ class SaaSQueryConfig(QueryConfig[SaaSRequestParams]):
             elif param.type == "path":
                 path = path.replace(f"<{param.name}>", input_data[param.name][0])
             elif param.type == "body":
-                if param.default_value:
-                    body = body.replace(f"<{param.name}>", f'"{param.default_value}"')
-                elif param.references or param.identity:
-                    body = body.replace(
-                        f"<{param.name}>", f'"{input_data[param.name][0]}"'
-                    )
-                else:
-                    logger.info(f"Missing body param value(s) for {path}")
-
+                body = SaaSQueryConfig._build_request_body(
+                    body,
+                    path,
+                    param.name,
+                    param.default_value,
+                    input_data[param.name][0] if param.references else None,
+                    input_data[param.name][0] if param.identity else None,
+                )
         logger.info(f"Populated request params for {current_request.path}")
         return "GET", path, query_params, json.loads(body) if body else None
 
@@ -106,7 +105,7 @@ class SaaSQueryConfig(QueryConfig[SaaSRequestParams]):
 
         path: str = current_request.path
         params: Dict[str, Any] = {}
-        custom_body: str = current_request.body or None
+        custom_body: Optional[str] = current_request.body or None
 
         # uses the reference fields to read from the param_values
         for param in current_request.request_params:
@@ -125,22 +124,18 @@ class SaaSQueryConfig(QueryConfig[SaaSRequestParams]):
                     pydash.get(param_values, param.references[0].field),
                 )
             elif param.type == "body":
-                if param.default_value:
-                    custom_body = custom_body.replace(
-                        f"<{param.name}>", f'"{param.default_value}"'
-                    )
-                elif param.references:
-                    custom_body = custom_body.replace(
-                        f"<{param.name}>",
-                        f'"{pydash.get(param_values, param.references[0].field)}"',
-                    )
-                elif param.identity:
-                    custom_body = custom_body.replace(
-                        f"<{param.name}>",
-                        f'"{pydash.get(param_values, param.identity)}"',
-                    )
-                else:
-                    logger.info(f"Missing body param value(s) for {path}")
+                custom_body = SaaSQueryConfig._build_request_body(
+                    custom_body,
+                    path,
+                    param.name,
+                    param.default_value,
+                    pydash.get(param_values, param.references[0].field)
+                    if param.references
+                    else None,
+                    pydash.get(param_values, param.identity)
+                    if param.identity
+                    else None,
+                )
 
         logger.info(f"Populated request params for {current_request.path}")
 
@@ -157,6 +152,36 @@ class SaaSQueryConfig(QueryConfig[SaaSRequestParams]):
             params,
             json.dumps(json.loads(custom_body) if custom_body else body),
         )
+
+    @staticmethod
+    def _build_request_body(  # pylint: disable=R0913
+        custom_body: Optional[str],
+        path: str,
+        param_name: str,
+        default_value: str = None,
+        field_reference: str = None,
+        identity: str = None,
+    ) -> Optional[str]:
+        """Method to build request body based on config vals. Common to both read and update requests."""
+        if not custom_body:
+            logger.info(f"Missing custom body {path}")
+            return None
+        if default_value:
+            custom_body = custom_body.replace(f"<{param_name}>", f'"{default_value}"')
+        elif field_reference:
+            custom_body = custom_body.replace(
+                f"<{param_name}>",
+                f'"{field_reference}"',
+            )
+        elif identity:
+            custom_body = custom_body.replace(
+                f"<{param_name}>",
+                f'"{identity}"',
+            )
+        else:
+            logger.info(f"Missing body param value(s) for {path}")
+            return None
+        return custom_body
 
     def query_to_str(self, t: T, input_data: Dict[str, List[Any]]) -> str:
         """Convert query to string"""
