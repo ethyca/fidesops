03a9b6e87399425f0020a9958ddbf1dc8b401aba
Adam Sachs <adam@Adams-MacBook-Pro.local>
2022-05-04 10:57:24 -0400

    erasure (update) support for sendgrid

--- tests/fixtures/saas/sendgrid_fixtures.py
+++ tests/fixtures/saas/sendgrid_fixtures.py
@@ -1,3 +1,4 @@
+from codecs import ignore_errors
 import json
 from fidesops.core.config import load_toml
 from fidesops.db import session
@@ -7,17 +8,27 @@ from fidesops.models.connectionconfig import (
     ConnectionType,
 )
 from fidesops.models.datasetconfig import DatasetConfig
+from fidesops.schemas.saas.shared_schemas import SaaSRequestParams, HTTPMethod
+from fidesops.service.connectors import SaaSConnector
+from fidesops.util import cryptographic_util
 import pytest
 import pydash
 import os
 from typing import Any, Dict, Generator
-from fidesops.schemas.saas.shared_schemas import HTTPMethod, SaaSRequestParams
-from fidesops.service.connectors.saas_connector import SaaSConnector
 from tests.fixtures.application_fixtures import load_dataset
 from tests.fixtures.saas_example_fixtures import load_config
 from sqlalchemy.orm import Session
+import time
+import requests
+from starlette.status import HTTP_202_ACCEPTED, HTTP_404_NOT_FOUND
 
 saas_config = load_toml("saas_config.toml")
+SENDGRID_ERASURE_FIRSTNAME = "Erasurefirstname"
+
+
+@pytest.fixture(scope="session")
+def sendgrid_erasure_identity_email():
+    return f"{cryptographic_util.generate_secure_random_string(13)}@email.com"
 
 
 @pytest.fixture(scope="function")
@@ -90,32 +101,115 @@ def sendgrid_dataset_config(
 
 
 @pytest.fixture(scope="function")
-def reset_sendgrid_data(
-    sendgrid_connection_config, sendgrid_identity_email
+def sendgrid_erasure_data(
+    sendgrid_connection_config, sendgrid_erasure_identity_email, sendgrid_secrets
 ) -> Generator:
     """
-    Gets the current value of the resource and restores it after the test is complete.
-    Used for erasure tests.
+    Creates a dynamic test data record for erasure tests.
+    Yields contact ID as this may be useful to have in test scenarios
     """
     connector = SaaSConnector(sendgrid_connection_config)
-    request_body = {"query": f"email = '{sendgrid_identity_email}'"}
-    request: SaaSRequestParams = SaaSRequestParams(
-        method=HTTPMethod.GET,
-        path="/v3/marketing/contacts/search",
-        body=json.dumps(request_body),
-    )
-    response = connector.create_client().send(request)
-    body = response.json()
-    contact = body["result"][0]
-    yield contact
-    request_body = {}
-    if "list_ids" in contact:
-        request_body["list_ids"] = contact["list_ids"]
-        contact.pop("list_ids", None)  # remove list_ids from contact object
-    request_body["contacts"] = [contact]
-    request: SaaSRequestParams = SaaSRequestParams(
+
+    # Create contact
+    body = {
+        "list_ids": ["62d20902-1cdd-42e7-8d5d-0fbb2a8be13e"],
+        "contacts": [
+            {
+                "address_line_1": "address_line_1",
+                "address_line_2": "address_line_2",
+                "city": "CITY (optional)",
+                "country": "country (optional)",
+                "email": sendgrid_erasure_identity_email,
+                "first_name": SENDGRID_ERASURE_FIRSTNAME,
+                "last_name": "Testcontact",
+                "postal_code": "postal_code (optional)",
+                "state_province_region": "state (optional)",
+                "custom_fields": {},
+            }
+        ],
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {sendgrid_secrets['api_key']}",
+    }
+    contacts_request: SaaSRequestParams = SaaSRequestParams(
         method=HTTPMethod.PUT,
         path="/v3/marketing/contacts",
-        body=json.dumps(request_body),
+        headers=headers,
+        body=json.dumps(body),
     )
-    connector.create_client().send(request)
+    contacts_response = connector.create_client().send(contacts_request)
+    assert HTTP_202_ACCEPTED == contacts_response.status_code
+
+    # Checks contact is propagated to Sendgrid before calling access / erasure requests
+    # this has taken over 25s in testing
+    remaining_tries = 10
+    while (
+        contact_id := _get_contact_id_if_exists(
+            sendgrid_erasure_identity_email, connector, sendgrid_secrets
+        )
+    ) is None:
+        remaining_tries -= 1
+        if remaining_tries < 1:
+            raise Exception(
+                f"Contact with email {sendgrid_erasure_identity_email} could not be added to Sendgrid"
+            )
+        time.sleep(5)
+
+    # yield contact_id becuase erasure email is already exposed via its own fixture
+    yield contact_id
+
+    delete_request: SaaSRequestParams = SaaSRequestParams(
+        method=HTTPMethod.DELETE,
+        path=f"/v3/marketing/contacts?ids={contact_id}",
+    )
+    connector.create_client().send(delete_request)
+
+    # verify contact is deleted
+    remaining_tries = 10
+    while (
+        contact_id := _get_contact_id_if_exists(
+            sendgrid_erasure_identity_email, connector, sendgrid_secrets
+        )
+    ) is not None:
+        remaining_tries -= 1
+        if remaining_tries < 1:
+            raise Exception(
+                f"Contact with contact id {contact_id}, email {sendgrid_erasure_identity_email} could not be deleted from Sendgrid"
+            )
+        time.sleep(5)
+
+
+def _get_contact_id_if_exists(
+    sendgrid_erasure_identity_email: str, connector: SaaSConnector, sendgrid_secrets
+) -> bool:
+    """
+    Confirm whether contact exists by calling contact search by email api and comparing resulting firstname str.
+    Returns contact ID if it exists, returns None if it does not.
+    """
+
+    body = json.dumps({"emails": [sendgrid_erasure_identity_email]})
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {sendgrid_secrets['api_key']}",
+    }
+
+    contact_request: SaaSRequestParams = SaaSRequestParams(
+        method=HTTPMethod.POST,
+        path="/v3/marketing/contacts/search/emails",
+        headers=headers,
+        body=body,
+    )
+    contact_response = connector.create_client().send(
+        contact_request, ignore_errors=True
+    )
+
+    # this handles when the contact doesn't exist
+    # really a 404 is returned, but the saas client catches the 404
+    # and instead just returns a totally empty response object
+    if None == contact_response.status_code:
+        return None
+
+    return contact_response.json()["result"][sendgrid_erasure_identity_email][
+        "contact"
+    ]["id"]
--- tests/integration_tests/saas/test_sendgrid_task.py
+++ tests/integration_tests/saas/test_sendgrid_task.py
@@ -1,13 +1,17 @@
 import pytest
 import random
+import json
+import time
 
 from fidesops.graph.graph import DatasetGraph
-from fidesops.models.privacy_request import ExecutionLog, PrivacyRequest
+from fidesops.models.privacy_request import PrivacyRequest
+from fidesops.service.connectors import SaaSConnector
 from fidesops.schemas.redis_cache import PrivacyRequestIdentity
+from fidesops.schemas.saas.shared_schemas import SaaSRequestParams, HTTPMethod
 
 from fidesops.task import graph_task
 from fidesops.task.graph_task import get_cached_data_for_erasures
-from tests.graph.graph_test_util import assert_rows_match, records_matching_fields
+from tests.graph.graph_test_util import assert_rows_match
 
 
 @pytest.mark.integration_saas
@@ -61,3 +65,125 @@ def test_sendgrid_access_request_task(
             "updated_at",
         ],
     )
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_sendgrid
+def test_sendgrid_erasure_request_task(
+    db,
+    policy,
+    erasure_policy_string_rewrite,
+    sendgrid_secrets,
+    sendgrid_connection_config,
+    sendgrid_dataset_config,
+    sendgrid_erasure_identity_email,
+    sendgrid_erasure_data,
+) -> None:
+    """Full erasure request based on the sendgrid SaaS config"""
+    privacy_request = PrivacyRequest(
+        id=f"test_saas_erasure_request_task_{random.randint(0, 1000)}"
+    )
+    identity = PrivacyRequestIdentity(**{"email": sendgrid_erasure_identity_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = sendgrid_connection_config.get_saas_config().fides_key
+    merged_graph = sendgrid_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    # access our erasure identity
+    v = graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [sendgrid_connection_config],
+        {"email": sendgrid_erasure_identity_email},
+    )
+
+    # make sure erasure contact has expected fields
+    assert_rows_match(
+        v[f"{dataset_name}:contacts"],
+        min_size=1,
+        keys=[
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "alternate_emails",
+            "address_line_1",
+            "address_line_2",
+            "city",
+            "state_province_region",
+            "country",
+            "postal_code",
+            "phone_number",
+            "whatsapp",
+            "list_ids",
+            "segment_ids",
+            "created_at",
+            "updated_at",
+        ],
+    )
+
+    erasure = graph_task.run_erasure(
+        privacy_request,
+        erasure_policy_string_rewrite,
+        graph,
+        [sendgrid_connection_config],
+        {"email": sendgrid_erasure_identity_email},
+        get_cached_data_for_erasures(privacy_request.id),
+    )
+
+    # masking request only issued to "contacts", for now
+    assert erasure == {f"{dataset_name}:contacts": 1}
+
+    # retrieve updated contact record, and verify its firstname is now masked
+    # update may take a while (>25s) to propagate, so retry up to 10 times
+    connector = SaaSConnector(sendgrid_connection_config)
+    remaining_tries = 10
+    while (
+        contact_firstname := _get_contact_firstname(
+            sendgrid_erasure_identity_email, connector, sendgrid_secrets
+        )
+    ) == "MASKED":
+        remaining_tries -= 1
+        if remaining_tries < 1:
+            raise Exception(
+                f"Contact with email {sendgrid_erasure_identity_email} was not updated in Sendgrid"
+            )
+        time.sleep(5)
+
+
+def _get_contact_firstname(
+    sendgrid_erasure_identity_email: str, connector: SaaSConnector, sendgrid_secrets
+) -> bool:
+    """
+    Retrieves contact with specified email from Sendgrid
+    Returns contact firstname if contact exists, returns None if it does not.
+    """
+    body = json.dumps({"emails": [sendgrid_erasure_identity_email]})
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {sendgrid_secrets['api_key']}",
+    }
+
+    contact_request: SaaSRequestParams = SaaSRequestParams(
+        method=HTTPMethod.POST,
+        path="/v3/marketing/contacts/search/emails",
+        headers=headers,
+        body=body,
+    )
+    contact_response = connector.create_client().send(
+        contact_request, ignore_errors=True
+    )
+
+    # this handles when the contact doesn't exist
+    # really a 404 is returned, but the saas client catches the 404
+    # and instead just returns a totally empty response object
+    if None == contact_response.status_code:
+        return None
+
+    # response has result object where keys are contact emails.
+    # confirm we get our contact back, and it has the right firstname
+    return contact_response.json()["result"][sendgrid_erasure_identity_email][
+        "contact"
+    ]["first_name"]
