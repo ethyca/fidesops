3649ec6f0fa950872fef00cd9f16bab70d153d28
Sean Preston <sean@ethyca.com>
2022-05-06 15:19:29 -0400

    remove boolean return type

--- src/fidesops/api/v1/endpoints/user_endpoints.py
+++ src/fidesops/api/v1/endpoints/user_endpoints.py
@@ -97,7 +97,7 @@ def create_user(
     return user
 
 
-def _validate_current_user(user_id: str, user_from_token: FidesopsUser) -> bool:
+def _validate_current_user(user_id: str, user_from_token: FidesopsUser) -> None:
     if not user_from_token:
         raise HTTPException(
             status_code=HTTP_404_NOT_FOUND,
@@ -109,7 +109,6 @@ def _validate_current_user(user_id: str, user_from_token: FidesopsUser) -> bool:
             status_code=HTTP_401_UNAUTHORIZED,
             detail=f"You are only authorised to update your own user data.",
         )
-    return True
 
 
 @router.put(
