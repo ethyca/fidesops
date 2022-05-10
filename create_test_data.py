ec76e4dd62bdca0b41412cacad240cee0b2f21ea
Sean Preston <sean@ethyca.com>
2022-05-10 17:24:35 -0400

    updates script to sync with policies the privacy centre expects to exist by default

--- create_test_data.py
+++ create_test_data.py
@@ -11,6 +11,12 @@ from fidesops.models.client import ClientDetail
 from fidesops.models.fidesops_user import FidesopsUser
 from fidesops.models.policy import ActionType, Policy, Rule, RuleTarget
 from fidesops.models.privacy_request import PrivacyRequest, PrivacyRequestStatus
+from fidesops.models.storage import StorageConfig, ResponseFormat
+from fidesops.schemas.storage.storage import (
+    FileNaming,
+    StorageDetails,
+    StorageType,
+)
 from fidesops.util.data_category import DataCategory
 
 
@@ -21,21 +27,29 @@ def _create_policy(
     db: orm.Session,
     action_type: str,
     client_id: str,
+    policy_key: str,
 ) -> Policy:
-    rand = string.ascii_lowercase[:5]
-    policy = Policy.create(
+    """
+    Util method to create policies
+    """
+    created, policy = Policy.get_or_create(
         db=db,
         data={
-            "name": f"example {action_type} policy {rand}",
-            "key": f"example_{action_type}_policy_{rand}",
+            "name": policy_key,
+            "key": policy_key,
             "client_id": client_id,
         },
     )
 
-    rule = Rule.create(
-        db=db,
-        data={
-            "action_type": ActionType.erasure.value,
+    if not created:
+        # If the Policy is already created, don't create it again
+        return policy
+
+    rand = string.ascii_lowercase[:5]
+    data = {}
+    if action_type == ActionType.erasure.value:
+        data = {
+            "action_type": action_type,
             "name": f"{action_type} Rule {rand}",
             "policy_id": policy.id,
             "masking_strategy": {
@@ -43,7 +57,32 @@ def _create_policy(
                 "configuration": {},
             },
             "client_id": client_id,
-        },
+        }
+    elif action_type == ActionType.access.value:
+        _, storage_config = StorageConfig.get_or_create(
+            db=db,
+            data={
+                "name": "test storage config",
+                "type": StorageType.s3,
+                "details": {
+                    StorageDetails.NAMING.value: FileNaming.request_id.value,
+                    StorageDetails.BUCKET.value: "test_bucket",
+                },
+                "key": f"storage_config_for_{policy_key}",
+                "format": ResponseFormat.json,
+            },
+        )
+        data = {
+            "action_type": action_type,
+            "name": f"{action_type} Rule {rand}",
+            "policy_id": policy.id,
+            "storage_destination_id": storage_config.id,
+            "client_id": client_id,
+        }
+
+    rule = Rule.create(
+        db=db,
+        data=data,
     )
 
     RuleTarget.create(
@@ -60,7 +99,7 @@ def _create_policy(
 def create_test_data(db: orm.Session) -> FidesopsUser:
     """Script to create test data for the Admin UI"""
     print(f"Seeding database with privacy requests")
-    client = ClientDetail.create(
+    _, client = ClientDetail.get_or_create(
         db=db,
         data={
             "fides_key": "ci_create_test_data",
@@ -70,9 +109,25 @@ def create_test_data(db: orm.Session) -> FidesopsUser:
         },
     )
 
-    for action in ActionType.__members__.values():
-        policy = _create_policy(db, action.value, client.id)
+    policies = []
+    policies.append(
+        _create_policy(
+            db=db,
+            action_type=ActionType.erasure.value,
+            client_id=client.id,
+            policy_key="delete",
+        )
+    )
+    policies.append(
+        _create_policy(
+            db=db,
+            action_type=ActionType.access.value,
+            client_id=client.id,
+            policy_key="download",
+        )
+    )
 
+    for policy in policies:
         for status in PrivacyRequestStatus.__members__.values():
             PrivacyRequest.create(
                 db=db,
