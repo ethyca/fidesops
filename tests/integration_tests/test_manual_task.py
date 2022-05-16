466965136ee0dff440b40c4a3c39f67ee3e8f8c2
Dawn Pattison <pattisdr@users.noreply.github.com>
2022-05-16 17:47:20 -0500

    Assert customer data in graph too.

--- tests/integration_tests/test_manual_task.py
+++ tests/integration_tests/test_manual_task.py
@@ -2,13 +2,11 @@ import random
 import pytest
 
 from fidesops.common_exceptions import PrivacyRequestPaused
-from fidesops.models.privacy_request import ExecutionLog, PrivacyRequest
+from fidesops.models.privacy_request import PrivacyRequest
 from fidesops.task import graph_task
-from fidesops.task.task_resources import TaskResources
 
 from ..graph.graph_test_util import (
     assert_rows_match,
-    records_matching_fields
 )
 from ..task.traversal_data import postgres_and_manual_graph
 
@@ -37,7 +35,8 @@ def test_postgres_with_manual_input_access_request_task(
             {"email": "customer-1@example.com"},
         )
 
-    # Graph is paused at filing cabinet.
+    # Graph is paused at the filing cabinet - user needs to manually retrieve some
+    # info and add to the graph
     paused_node = privacy_request.get_cached_paused_node()
     assert paused_node == "manual_example:filing_cabinet"
 
@@ -46,6 +45,7 @@ def test_postgres_with_manual_input_access_request_task(
         f"MANUAL_INPUT__{privacy_request.id}__access_request__manual_example:filing_cabinet", [{"id": 1, "beneficiary": "Jane Doe"}]
     )
 
+    # Restart the same privacy request from the paused node
     v = graph_task.run_access_request(
         privacy_request,
         policy,
@@ -54,9 +54,14 @@ def test_postgres_with_manual_input_access_request_task(
         {"email": "customer-1@example.com"},
         from_paused=True
     )
-
     assert_rows_match(
         v["manual_example:filing_cabinet"],
         min_size=1,
         keys=["id", "beneficiary"],
+    )
+
+    assert_rows_match(
+        v["postgres_example:customer"],
+        min_size=1,
+        keys=["id", "name", "email", "address_id"],
     )
