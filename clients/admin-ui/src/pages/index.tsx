f8a92e9dde9ba5ce4100d266a86fbaf9170d5406
Elliot Bonneville <elliot.bonneville@gmail.com>
2022-05-24 12:47:25 -0400

    Add empty props object to fix failing return

--- clients/admin-ui/src/pages/index.tsx
+++ clients/admin-ui/src/pages/index.tsx
@@ -10,26 +10,26 @@ import RequestTable from '../features/privacy-requests/RequestTable';
 import { assignToken, setUser } from '../features/user/user.slice';
 
 const Home: NextPage = () => (
-    <div>
-      <Head>
-        <title>Fides Admin UI</title>
-        <meta name="description" content="Generated from FidesUI template" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  <div>
+    <Head>
+      <title>Fides Admin UI</title>
+      <meta name="description" content="Generated from FidesUI template" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <NavBar />
+    <NavBar />
 
-      <main>
-        <Box px={9} py={10}>
-          <Heading mb={8} fontSize="2xl" fontWeight="semibold">
-            Subject Requests
-          </Heading>
-          <RequestFilters />
-          <RequestTable />
-        </Box>
-      </main>
-    </div>
-  );
+    <main>
+      <Box px={9} py={10}>
+        <Heading mb={8} fontSize="2xl" fontWeight="semibold">
+          Subject Requests
+        </Heading>
+        <RequestFilters />
+        <RequestTable />
+      </Box>
+    </main>
+  </div>
+);
 
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async (context) => {
@@ -38,7 +38,7 @@ export const getServerSideProps = wrapper.getServerSideProps(
     if (session && typeof session.accessToken !== 'undefined') {
       await store.dispatch(assignToken(session.accessToken));
       await store.dispatch(setUser(session.user));
-      return {};
+      return { props: {} };
     }
 
     return {
