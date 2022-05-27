a1e054bcd496cb3bb109bea7e9e5f4edabe1928d
Elliot Bonneville <elliot.bonneville@gmail.com>
2022-05-27 17:10:34 -0400

    Remove unnecessary API files

--- clients/admin-ui/__tests__/index.test.tsx
+++ clients/admin-ui/__tests__/index.test.tsx
@@ -1,16 +1,10 @@
 // __tests__/index.test.tsx
-import { SessionProvider } from 'next-auth/react';
-
 import Home from '../src/pages/index';
 import { render, screen } from './test-utils';
 
 describe('Home', () => {
   it('renders the Subject Requests page by default', () => {
-    render(
-      <SessionProvider>
-        <Home />
-      </SessionProvider>
-    );
+    render(<Home />);
 
     const message = screen.getAllByText('Subject Requests')[0];
     expect(message).toBeInTheDocument();
