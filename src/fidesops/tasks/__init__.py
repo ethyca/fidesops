e0d4819dd38ca473a5a7158a409495b66ac187f5
Sean Preston <sean@ethyca.com>
2022-06-01 10:54:47 -0400

    adds celery worker to project and code to dispatch tasks

---
+++ src/fidesops/tasks/__init__.py
@@ -0,0 +1,10 @@
+from celery import Celery
+
+
+app = Celery("tasks")
+app.config_from_object("fidesops.core.config.settings", namespace="EXECUTION")
+app.autodiscover_tasks(["fidesops.tasks", "fidesops.tasks.scheduled"])
+
+
+if __name__ == "__main__":
+    app.worker_main()
