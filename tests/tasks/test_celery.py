9692e829d6babd1a6dcc2c54e6a3aa801517009e
Sean Preston <sean@ethyca.com>
2022-06-08 12:27:22 -0400

    elaborate on comment

--- tests/tasks/test_celery.py
+++ tests/tasks/test_celery.py
@@ -3,6 +3,8 @@ def test_create_task(celery_app, celery_worker):
     def multiply(x, y):
         return x * y
 
-    celery_worker.reload()  # Force `celery_app` to register our new task
+    # Force `celery_app` to register our new task
+    # See: https://github.com/celery/celery/issues/3642#issuecomment-369057682
+    celery_worker.reload()
     assert multiply.run(4, 4) == 16
     assert multiply.delay(4, 4).get(timeout=10) == 16
