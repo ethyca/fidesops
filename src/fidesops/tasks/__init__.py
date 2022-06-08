1067cf1847a4e183e797795e8c47a3fc0fc1378c
Sean Preston <sean@ethyca.com>
2022-06-08 17:37:44 -0400

    fix task config path

--- src/fidesops/tasks/__init__.py
+++ src/fidesops/tasks/__init__.py
@@ -1,7 +1,7 @@
 from celery import Celery
 
 app = Celery("tasks")
-app.config_from_object("fidesops.core.config.settings", namespace="EXECUTION")
+app.config_from_object("fidesops.core.config", namespace="EXECUTION")
 app.autodiscover_tasks(["fidesops.tasks", "fidesops.tasks.scheduled"])
 
 
