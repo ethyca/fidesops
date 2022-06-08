b4317cac63f1cb8695f5fff9774d52f69eb3a220
Sean Preston <sean@ethyca.com>
2022-06-08 19:29:43 -0400

    register new task

--- src/fidesops/tasks/__init__.py
+++ src/fidesops/tasks/__init__.py
@@ -2,7 +2,13 @@ from celery import Celery
 
 app = Celery("tasks")
 app.config_from_object("fidesops.core.config", namespace="EXECUTION")
-app.autodiscover_tasks(["fidesops.tasks", "fidesops.tasks.scheduled"])
+app.autodiscover_tasks(
+    [
+        "fidesops.tasks",
+        "fidesops.tasks.scheduled",
+        "fidesops.service.privacy_request.request_runner_service",
+    ]
+)
 
 
 if __name__ == "__main__":
