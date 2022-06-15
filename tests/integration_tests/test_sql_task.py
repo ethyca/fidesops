1b1af46996f36b8b03e1125bd51e0430c98078b0
eastandwestwind <eastandwestwind@gmail.com>
2022-06-15 08:39:42 -0400

    adds dev mode false to additional tests

--- tests/integration_tests/test_sql_task.py
+++ tests/integration_tests/test_sql_task.py
@@ -956,6 +956,7 @@ class TestRetryIntegration:
         policy,
         integration_postgres_config,
     ):
+        config.dev_mode = False
         config.execution.TASK_RETRY_COUNT = 1
         config.execution.TASK_RETRY_DELAY = 0.1
         config.execution.TASK_RETRY_BACKOFF = 0.01
@@ -1007,6 +1008,7 @@ class TestRetryIntegration:
         policy,
         integration_postgres_config,
     ):
+        config.dev_mode = False
         config.execution.TASK_RETRY_COUNT = 2
         config.execution.TASK_RETRY_DELAY = 0.1
         config.execution.TASK_RETRY_BACKOFF = 0.01
