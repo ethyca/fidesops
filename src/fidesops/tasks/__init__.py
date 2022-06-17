7602989bcfec487e4fb1e38898171655e7afc1e3
Sean Preston <sean@ethyca.com>
2022-06-17 15:01:21 -0400

    formatting

--- src/fidesops/tasks/__init__.py
+++ src/fidesops/tasks/__init__.py
@@ -2,8 +2,16 @@ from celery import Celery
 
 
 app = Celery("tasks")
-app.config_from_object("fidesops.core.config", namespace="EXECUTION")
-app.autodiscover_tasks(["fidesops.tasks", "fidesops.tasks.scheduled"])
+app.config_from_object(
+    "fidesops.core.config",
+    namespace="EXECUTION",
+)
+app.autodiscover_tasks(
+    [
+        "fidesops.tasks",
+        "fidesops.tasks.scheduled",
+    ]
+)
 
 
 def start_worker():
