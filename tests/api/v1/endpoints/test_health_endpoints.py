cd2def6d6b32f6f44d41a13c4ba607fc7a334d4f
eastandwestwind <eastandwestwind@gmail.com>
2022-06-21 11:24:12 -0400

    move health endpoint changes to separate branch

--- tests/api/v1/endpoints/test_health_endpoints.py
+++ tests/api/v1/endpoints/test_health_endpoints.py
@@ -5,4 +5,4 @@ from fidesops.api.v1.urn_registry import HEALTH
 
 def test_health(api_client: TestClient) -> None:
     response = api_client.get(HEALTH)
-    assert response.json() == {"healthy": True, "database": "healthy", "cache": "healthy"}
+    assert response.json() == {"healthy": True}
