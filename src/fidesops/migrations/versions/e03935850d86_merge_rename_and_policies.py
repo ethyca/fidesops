f9ba8e14409c5f1dad8369428b6c7bb364cc026d
Paul Sanders <pau@ethyca.com>
2022-06-24 21:16:45 -0400

    Run black and isort

--- src/fidesops/migrations/versions/e03935850d86_merge_rename_and_policies.py
+++ src/fidesops/migrations/versions/e03935850d86_merge_rename_and_policies.py
@@ -5,13 +5,12 @@ Revises: 46f7a577b933, 55d61eb8ed12
 Create Date: 2022-06-24 12:31:43.540964
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = 'e03935850d86'
-down_revision = ('46f7a577b933', '55d61eb8ed12')
+revision = "e03935850d86"
+down_revision = ("46f7a577b933", "55d61eb8ed12")
 branch_labels = None
 depends_on = None
 
