ac6c24d7ca01efd5fe85f65a4aa4b42c3917c684
eastandwestwind <eastandwestwind@gmail.com>
2022-07-13 10:13:41 -0500

    fix path for importing test helpers

--- tests/fixtures/saas/stripe_fixtures.py
+++ tests/fixtures/saas/stripe_fixtures.py
@@ -13,7 +13,7 @@ from fidesops.models.connectionconfig import (
 )
 from fidesops.models.datasetconfig import DatasetConfig
 from fidesops.util.saas_util import load_config
-from test_helpers.vault_client import get_secrets
+from tests.test_helpers.vault_client import get_secrets
 from tests.fixtures.application_fixtures import load_dataset
 
 secrets = get_secrets("stripe")
