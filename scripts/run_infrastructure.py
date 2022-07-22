80314a9e0afcef04e3b8cbddfa78ad7992f1b6ee
Paul Sanders <pau@ethyca.com>
2022-07-22 19:07:49 -0400

    Revert extra logging and extended timeout

--- scripts/run_infrastructure.py
+++ scripts/run_infrastructure.py
@@ -66,8 +66,7 @@ def run_infrastructure(
     else:
         _run_cmd_or_err(f"docker-compose {path} build")
 
-    # _run_cmd_or_err(f"docker-compose {path} up -d")
-    _run_cmd_or_err(f"docker-compose {path} up")
+    _run_cmd_or_err(f"docker-compose {path} up -d")
 
     wait = min(DOCKER_WAIT * len(datastores), 15)
     print(f"Sleeping for: {wait} while infrastructure loads...")
