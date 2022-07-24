43f5a736958f719f08e1ee3b76832dbd4dc57410
Paul Sanders <pau@ethyca.com>
2022-07-24 13:17:16 -0400

    Revert mssql only

--- scripts/run_infrastructure.py
+++ scripts/run_infrastructure.py
@@ -260,7 +260,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        # "-d",
         "--datastores",
         action="extend",
         nargs="*",
