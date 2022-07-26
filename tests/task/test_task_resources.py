88504cdba6eb8a32adc788df07d0a72be56a383a
Dawn Pattison <pattisdr@users.noreply.github.com>
2022-07-26 14:52:08 -0400

    Use missing db fixture.

--- tests/task/test_task_resources.py
+++ tests/task/test_task_resources.py
@@ -2,7 +2,9 @@ from fidesops.task.task_resources import TaskResources
 
 
 class TestTaskResources:
-    def test_cache_erasure(self, privacy_request, policy, integration_manual_config):
+    def test_cache_erasure(
+        self, db, privacy_request, policy, integration_manual_config
+    ):
         resources = TaskResources(
             privacy_request, policy, [integration_manual_config], db
         )
