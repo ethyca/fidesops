7a815721a3136812edc18593acacb2279d318040
noonari <sadaqatullah.noonari@gmail.com>
2022-08-11 16:39:35 +0500

    Tests for Connector config completed and tested locally.

--- tests/ops/integration_tests/saas/test_datadog_task.py
+++ tests/ops/integration_tests/saas/test_datadog_task.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import random
 
 import pytest
@@ -13,6 +14,9 @@ from fidesops.task.filter_results import filter_data_categories
 from fidesops.task.graph_task import get_cached_data_for_erasures
 from fidesops.util.saas_util import format_body
 from tests.ops.graph.graph_test_util import assert_rows_match, records_matching_fields
+
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.mark.integration_saas
@@ -38,6 +42,9 @@ def test_saas_access_request_task(
     dataset_name = datadog_connection_config.get_saas_config().fides_key
     merged_graph = dataset_config_datadog.get_graph()
     graph = DatasetGraph(merged_graph)
+
+    # import pdb; pdb.set_trace()
+
     v = graph_task.run_access_request(
         privacy_request,
         policy,
@@ -46,3 +53,33 @@ def test_saas_access_request_task(
         {"email": datadog_identity_email},
         db,
     )
+
+    assert_rows_match(
+        v[f"{dataset_name}:events"],
+        min_size=1,
+        keys=[
+            "attributes",
+            "type",
+            "id",
+        ]
+    )
+
+    for item in v[f"{dataset_name}:events"]:
+        assert_rows_match(
+            [item['attributes']],
+            min_size=1,
+            keys=[
+                "status",
+                "timestamp",
+                "message",
+                "tags",
+            ]
+        )
+    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
+    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
+    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
+    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
+    logger.info(msg=f"{len(v[f'{dataset_name}:events'])}")
+    for item in v[f"{dataset_name}:events"]:
+        assert datadog_identity_email in item["attributes"]["message"]
+
