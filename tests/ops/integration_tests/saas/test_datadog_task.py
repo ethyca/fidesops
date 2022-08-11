535f426a6e803e6dd576f5396b8110f57d9dbf7b
noonari <sadaqatullah.noonari@gmail.com>
2022-08-11 16:39:36 +0500

    Reformatted Code

--- tests/ops/integration_tests/saas/test_datadog_task.py
+++ tests/ops/integration_tests/saas/test_datadog_task.py
@@ -1,19 +1,13 @@
-import json
 import logging
 import random
 
 import pytest
 
 from fidesops.graph.graph import DatasetGraph
-from fidesops.models.privacy_request import ExecutionLog, PrivacyRequest
+from fidesops.models.privacy_request import PrivacyRequest
 from fidesops.schemas.redis_cache import PrivacyRequestIdentity
-from fidesops.schemas.saas.shared_schemas import HTTPMethod, SaaSRequestParams
-from fidesops.service.connectors import SaaSConnector
 from fidesops.task import graph_task
-from fidesops.task.filter_results import filter_data_categories
-from fidesops.task.graph_task import get_cached_data_for_erasures
-from fidesops.util.saas_util import format_body
-from tests.ops.graph.graph_test_util import assert_rows_match, records_matching_fields
+from tests.ops.graph.graph_test_util import assert_rows_match
 
 logger = logging.getLogger(__name__)
 
