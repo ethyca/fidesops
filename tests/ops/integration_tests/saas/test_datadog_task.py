bce8f14ccb13522799fe4a30fb681a5754c7c069
noonari <sadaqatullah.noonari@gmail.com>
2022-08-11 16:39:36 +0500

    Reformatted Code

--- tests/ops/integration_tests/saas/test_datadog_task.py
+++ tests/ops/integration_tests/saas/test_datadog_task.py
@@ -15,18 +15,17 @@ from fidesops.task.graph_task import get_cached_data_for_erasures
 from fidesops.util.saas_util import format_body
 from tests.ops.graph.graph_test_util import assert_rows_match, records_matching_fields
 
-
 logger = logging.getLogger(__name__)
 
 
 @pytest.mark.integration_saas
 @pytest.mark.integration_datadog
 def test_saas_access_request_task(
-    db,
-    policy,
-    datadog_connection_config,
-    dataset_config_datadog,
-    datadog_identity_email,
+        db,
+        policy,
+        datadog_connection_config,
+        dataset_config_datadog,
+        datadog_identity_email,
 ) -> None:
     """Full access request based on the Hubspot SaaS config"""
 
@@ -42,9 +41,6 @@ def test_saas_access_request_task(
     dataset_name = datadog_connection_config.get_saas_config().fides_key
     merged_graph = dataset_config_datadog.get_graph()
     graph = DatasetGraph(merged_graph)
-
-    # import pdb; pdb.set_trace()
-
     v = graph_task.run_access_request(
         privacy_request,
         policy,
@@ -75,11 +71,6 @@ def test_saas_access_request_task(
                 "tags",
             ]
         )
-    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
-    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
-    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
-    logger.info(msg=v[f"{dataset_name}:events"][0]['attributes']['timestamp'])
-    logger.info(msg=f"{len(v[f'{dataset_name}:events'])}")
+
     for item in v[f"{dataset_name}:events"]:
         assert datadog_identity_email in item["attributes"]["message"]
-
