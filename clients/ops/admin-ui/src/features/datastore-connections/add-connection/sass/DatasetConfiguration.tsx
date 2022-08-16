aeec408bd120823ee9040d701645c4bc629b2e1d
chriscalhoun1974 <68459950+chriscalhoun1974@users.noreply.github.com>
2022-08-16 19:33:16 -0400

    Resolved npm build issue

---
+++ clients/ops/admin-ui/src/features/datastore-connections/add-connection/sass/DatasetConfiguration.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type DatasetConfigurationProps = {};
+
+// eslint-disable-next-line no-empty-pattern
+const ConfigureConnector: React.FC<DatasetConfigurationProps> = ({}) => (
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  <></>
+);
+
+export default ConfigureConnector;
