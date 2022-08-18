0752f4d53b5f6e70d85a1fbcd7460da87d6bc3bb
chriscalhoun1974 <68459950+chriscalhoun1974@users.noreply.github.com>
2022-08-18 14:38:51 -0400

    Removed unnecessary import statement

--- clients/ops/admin-ui/src/features/datastore-connections/add-connection/ChooseConnection.tsx
+++ clients/ops/admin-ui/src/features/datastore-connections/add-connection/ChooseConnection.tsx
@@ -9,7 +9,6 @@ import {
 } from "@fidesui/react";
 import { SearchLineIcon } from "common/Icon";
 import { debounce } from "common/utils";
-import { SystemType } from "datastore-connections/constants";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 
