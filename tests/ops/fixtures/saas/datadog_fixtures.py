ffa4f55fabc5c7da73b8a981fc38d8d18699ad38
noonari <sadaqatullah.noonari@gmail.com>
2022-09-09 02:41:13 +0500

    PR feedbacks

--- tests/ops/fixtures/saas/datadog_fixtures.py
+++ tests/ops/fixtures/saas/datadog_fixtures.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, Generator
 
 import pydash
 import pytest
+import requests
 from fideslib.db import session
 from sqlalchemy.orm import Session
 
@@ -95,3 +96,35 @@ def datadog_dataset_config(
     )
     yield dataset
     dataset.delete(db=db)
+
+
+@pytest.fixture(scope="session")
+def datadog_access_data(datadog_secrets, datadog_identity_email):
+    url = "https://api.datadoghq.com/api/v2/logs/events?filter[from]=0&filter[query]=noonari@xyz.com&filter[to]=now&page[limit]=1000"
+
+    payload = {}
+    headers = {
+        "DD-API-KEY": f'{datadog_secrets.get("api_key")}',
+        "DD-APPLICATION-KEY": f'{datadog_secrets.get("app_key")}',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    if datadog_identity_email not in response.text:
+        url = "https://http-intake.logs.datadoghq.com/api/v2/logs"
+        payload = [
+            {
+                "ddsource": "nginx",
+                "ddtags": "env:staging,version:5.1",
+                "hostname": "i-012345678",
+                "message": f"2019-11-19T14:37:58,995 INFO [process.name][20081{i}] Hello noonari@xyz.com",
+                "service": "payment",
+            }
+            for i in range(25)
+        ]
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "DD-API-KEY": f'{datadog_secrets.get("api_key")}',
+        }
+        requests.request("POST", url, headers=headers, json=payload)
