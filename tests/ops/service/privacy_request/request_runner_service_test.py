1dcf9ac8bbf10c9ecccaa2dfb25fe8068782ba69
eastandwestwind <eastandwestwind@gmail.com>
2022-09-09 17:01:43 -0500

    move fixture to class level

--- tests/ops/service/privacy_request/request_runner_service_test.py
+++ tests/ops/service/privacy_request/request_runner_service_test.py
@@ -62,15 +62,6 @@ PRIVACY_REQUEST_TASK_TIMEOUT = 5
 PRIVACY_REQUEST_TASK_TIMEOUT_EXTERNAL = 30
 
 
-@pytest.fixture(scope="function")
-def privacy_request_complete_email_notification_enabled():
-    """Enable request completion email"""
-    original_value = config.notifications.send_request_completion_notification
-    config.notifications.send_request_completion_notification = True
-    yield
-    config.notifications.send_request_completion_notification = original_value
-
-
 @mock.patch(
     "fidesops.ops.service.privacy_request.request_runner_service.dispatch_email"
 )
@@ -1838,6 +1829,14 @@ class TestPrivacyRequestsEmailConnector:
 
 
 class TestPrivacyRequestsEmailNotifications:
+    @pytest.fixture(scope="function")
+    def privacy_request_complete_email_notification_enabled():
+        """Enable request completion email"""
+        original_value = config.notifications.send_request_completion_notification
+        config.notifications.send_request_completion_notification = True
+        yield
+        config.notifications.send_request_completion_notification = original_value
+
     @pytest.mark.integration_postgres
     @pytest.mark.integration
     @mock.patch(
