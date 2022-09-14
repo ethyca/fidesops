785131c126b2ae6ffeb4d8f0ee4acbc4940106ca
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-09-14 15:53:41 -0400

    Implement PR feedback

--- clients/ops/admin-ui/src/features/privacy-requests/SortRequestButton.tsx
+++ clients/ops/admin-ui/src/features/privacy-requests/SortRequestButton.tsx
@@ -1,4 +1,5 @@
-import { Flex, Icon, IconButton } from "@fidesui/react";
+import { Flex, IconButton } from "@fidesui/react";
+import { SortArrowIcon } from "common/Icon";
 import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -30,6 +31,7 @@ const useSortRequestButton = ({
     ButtonState.UNSELECTED
   );
   const [wasButtonJustClicked, setWasButtonJustClicked] = useState(false);
+
   useEffect(() => {
     if (!isLoading) {
       setWasButtonJustClicked(false);
@@ -45,19 +47,22 @@ const useSortRequestButton = ({
   const handleButtonClick = useCallback(() => {
     setWasButtonJustClicked(true);
     dispatch(setSortField(sortField));
-    if (buttonState === ButtonState.UNSELECTED) {
-      dispatch(setSortDirection(ButtonState.ASC));
-      setButtonState(ButtonState.ASC);
-    }
 
-    if (buttonState === ButtonState.ASC) {
-      dispatch(setSortDirection(ButtonState.DESC));
-      setButtonState(ButtonState.DESC);
-    }
-
-    if (buttonState === ButtonState.DESC) {
-      dispatch(clearSortFields());
-      setButtonState(ButtonState.UNSELECTED);
+    switch (buttonState) {
+      case ButtonState.UNSELECTED:
+        dispatch(setSortDirection(ButtonState.ASC));
+        setButtonState(ButtonState.ASC);
+        break;
+      case ButtonState.ASC:
+        dispatch(setSortDirection(ButtonState.DESC));
+        setButtonState(ButtonState.DESC);
+        break;
+      case ButtonState.DESC:
+        dispatch(clearSortFields());
+        setButtonState(ButtonState.UNSELECTED);
+        break;
+      default:
+        break;
     }
   }, [buttonState, setButtonState, dispatch, sortField]);
 
@@ -66,173 +71,6 @@ const useSortRequestButton = ({
     buttonState,
     wasButtonJustClicked,
   };
-};
-
-type ArrowIconProps = {
-  up?: boolean;
-};
-
-const ArrowIcon: React.FC<ArrowIconProps> = ({ up }) => {
-  if (up === undefined) {
-    return (
-      <Icon width="24px" height="26px" viewBox="0 0 24 26" fill="none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="26"
-          viewBox="0 0 24 26"
-          fill="none"
-        >
-          <path
-            d="M8.72726 15.129L10.9716 12.8145L12 13.875L7.99998 18L4 13.875L5.02837 12.8145L7.27271 15.129V6H8.72726V15.129Z"
-            fill="#2D3748"
-          />
-          <path
-            d="M16.7523 8.871V18H15.3089V8.871L13.0205 11.25L12 10.1895L16.0306 6L20 10.125L18.9795 11.1855L16.7523 8.871Z"
-            fill="#2D3748"
-          />
-        </svg>
-      </Icon>
-    );
-  }
-
-  if (up) {
-    return (
-      <Icon
-        width="24px"
-        height="26px"
-        viewBox="0 0 24 26"
-        fill="none"
-        style={{
-          transform: "rotate(180deg)",
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="26"
-          viewBox="0 0 24 26"
-          fill="none"
-        >
-          <g filter="url(#filter0_d_3399_48680)">
-            <path
-              d="M8.72726 15.129L10.9716 12.8145L12 13.875L7.99998 18L4 13.875L5.02837 12.8145L7.27271 15.129V6H8.72726V15.129Z"
-              fill="#2D3748"
-            />
-            <path
-              d="M8.22726 15.129V16.3629L9.08621 15.4771L10.9716 13.5327L11.3035 13.875L7.99998 17.2818L4.69648 13.875L5.02837 13.5327L6.91375 15.4771L7.77271 16.3629V15.129V6.5H8.22726V15.129Z"
-              stroke="black"
-            />
-          </g>
-          <path
-            d="M16.7523 8.871V18H15.3089V8.871L13.0205 11.25L12 10.1895L16.0306 6L20 10.125L18.9795 11.1855L16.7523 8.871Z"
-            fill="#2D3748"
-          />
-          <defs>
-            <filter
-              id="filter0_d_3399_48680"
-              x="0"
-              y="6"
-              width="16"
-              height="20"
-              filterUnits="userSpaceOnUse"
-              colorInterpolationFilters="sRGB"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                result="hardAlpha"
-              />
-              <feOffset dy="4" />
-              <feGaussianBlur stdDeviation="2" />
-              <feComposite in2="hardAlpha" operator="out" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="BackgroundImageFix"
-                result="effect1_dropShadow_3399_48680"
-              />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="effect1_dropShadow_3399_48680"
-                result="shape"
-              />
-            </filter>
-          </defs>
-        </svg>
-      </Icon>
-    );
-  }
-
-  return (
-    <Icon width="24px" height="26px" viewBox="0 0 24 26" fill="none">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="26"
-        viewBox="0 0 24 26"
-        fill="none"
-      >
-        <g filter="url(#filter0_d_3399_48680)">
-          <path
-            d="M8.72726 15.129L10.9716 12.8145L12 13.875L7.99998 18L4 13.875L5.02837 12.8145L7.27271 15.129V6H8.72726V15.129Z"
-            fill="#2D3748"
-          />
-          <path
-            d="M8.22726 15.129V16.3629L9.08621 15.4771L10.9716 13.5327L11.3035 13.875L7.99998 17.2818L4.69648 13.875L5.02837 13.5327L6.91375 15.4771L7.77271 16.3629V15.129V6.5H8.22726V15.129Z"
-            stroke="black"
-          />
-        </g>
-        <path
-          d="M16.7523 8.871V18H15.3089V8.871L13.0205 11.25L12 10.1895L16.0306 6L20 10.125L18.9795 11.1855L16.7523 8.871Z"
-          fill="#2D3748"
-        />
-        <defs>
-          <filter
-            id="filter0_d_3399_48680"
-            x="0"
-            y="6"
-            width="16"
-            height="20"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dy="4" />
-            <feGaussianBlur stdDeviation="2" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_3399_48680"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_3399_48680"
-              result="shape"
-            />
-          </filter>
-        </defs>
-      </svg>
-    </Icon>
-  );
 };
 
 type SortRequestButtonProps = {
@@ -251,16 +89,16 @@ const SortRequestButton: React.FC<SortRequestButtonProps> = ({
 
   switch (buttonState) {
     case ButtonState.ASC:
-      icon = <ArrowIcon up />;
+      icon = <SortArrowIcon up />;
       break;
     case ButtonState.DESC:
-      icon = <ArrowIcon up={false} />;
+      icon = <SortArrowIcon up={false} />;
       break;
     case ButtonState.UNSELECTED:
-      icon = <ArrowIcon />;
+      icon = <SortArrowIcon />;
       break;
     default:
-      icon = <ArrowIcon />;
+      icon = <SortArrowIcon />;
   }
 
   return (
