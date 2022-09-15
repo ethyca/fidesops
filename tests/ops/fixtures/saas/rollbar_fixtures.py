05c495523c47c253a27d4fc0fd5b944fc084d935
Zeeshan <zmuhammad@onbench.com>
2022-09-16 04:30:45 +0500

    add erasure api. ref: 798-rollbar

--- tests/ops/fixtures/saas/rollbar_fixtures.py
+++ tests/ops/fixtures/saas/rollbar_fixtures.py
@@ -115,52 +115,142 @@ def rollbar_erasure_identity_email():
     return f"{cryptographic_util.generate_secure_random_string(13)}@email.com"
 
 
+class RollbarTestClient:
+
+    headers: object = {}
+    base_url: str = ""
+    rollbar_secrets: object = {}
+
+    def __init__(self, rollbar_connection_config: ConnectionConfig):
+        self.rollbar_secrets = rollbar_connection_config.secrets
+        self.headers = {
+            "Content-Type": "application/json",
+            "X-Rollbar-Access-Token": self.rollbar_secrets["read_access_token"],
+        }
+        self.base_url = f"https://{self.rollbar_secrets['domain']}/api/1"
+
+    def create_project(self) -> requests.Response:
+        # create a new project
+        import pdb
+
+        pdb.set_trace()
+        random_num = random.randint(0, 999)
+        body = {"name": f"ethyca_test_project_{random_num}"}
+        self.headers["X-Rollbar-Access-Token"] = self.rollbar_secrets[
+            "write_access_token"
+        ]
+
+        project_response: requests.Response = requests.post(
+            url=f"{self.base_url}/projects", json=body, headers=self.headers
+        )
+        return project_response
+
+    def get_project(self, project_id: str) -> requests.Response:
+        # get a project
+        self.headers["X-Rollbar-Access-Token"] = self.rollbar_secrets[
+            "read_access_token"
+        ]
+        #     Use an Account Access Token with 'read' scope
+        project_response: requests.Response = requests.get(
+            url=f"{self.base_url}/project/{project_id}/access_tokens",
+            headers=self.headers,
+        )
+        return project_response
+
+    def create_item(self, project_tokens: dict, email: str) -> requests.Response:
+        # create item inside above created project
+        self.headers["X-Rollbar-Access-Token"] = project_tokens["post_server_item"]
+        # Use an access token with scope post_server_item
+        body = {
+            "data": {
+                "environment": "production",
+                "body": {
+                    "message": {
+                        "body": "Request over threshold of 10 seconds",
+                        "route": "home#index",
+                        "time_elapsed": 15.23,
+                    },
+                    "level": "error",
+                },
+                "person": {
+                    "id": "1",
+                    "username": "ethyca",
+                    "email": email,
+                },
+            }
+        }
+        item_response: requests.Response = requests.post(
+            url=f"{self.base_url}/item/", json=body, headers=self.headers
+        )
+        return item_response
+
+    def get_item(self, project_tokens: dict) -> requests.Response:
+        # List all items
+        self.headers["X-Rollbar-Access-Token"] = project_tokens["read"]
+        item_response: requests.Response = requests.get(
+            url=f"{self.base_url}/items", headers=self.headers
+        )
+        return item_response
+
+
 @pytest.fixture(scope="function")
-def rollbar_create_erasure_data(
-    rollbar_connection_config: ConnectionConfig, rollbar_erasure_identity_email: str
+def rollbar_test_client(
+    rollbar_connection_config: RollbarTestClient,
+) -> Generator:
+    test_client = RollbarTestClient(rollbar_connection_config=rollbar_connection_config)
+    yield test_client
+
+
+def _project_exists(project_id: str, rollbar_test_client: RollbarTestClient) -> Any:
+    """ """
+    project_response = rollbar_test_client.get_project(project_id=project_id)
+
+    if not project_response.status_code == 404 and project_response.json()["result"]:
+        return project_response.json()
+
+
+def _item_exists(project_tokens, rollbar_test_client: RollbarTestClient) -> Any:
+    """ """
+    item_response = rollbar_test_client.get_item(project_tokens=project_tokens)
+
+    if not item_response.status_code == 404 and item_response.json()["result"]:
+        return item_response.json()
+
+
+@pytest.fixture(scope="function")
+def rollbar_erasure_data(
+    rollbar_test_client: RollbarTestClient, rollbar_erasure_identity_email: str
 ) -> Generator:
     """
     Creates a dynamic test data record for erasure tests.
     """
-    rollbar_secrets = rollbar_connection_config.secrets
     """
         there are 3 steps to create data for erasure api
         1) create a new project
         2) get access token with scope post_server_item from project details
         3) create item inside above created project
-        """
+    """
 
     # 1) create a new project
-    random_num = random.randint(0, 99)
-    base_url = f"https://{rollbar_secrets['domain']}"
-    body = {"name": f"ethyca_test_project_{random_num}"}
-    headers = {
-        "Content-Type": "application/json",
-        "X-Rollbar-Access-Token": rollbar_secrets["write_access_token"],
-        # Use an Account Access Token with 'write' scope
-    }
-
-    project_response = requests.post(
-        url=f"{base_url}/api/1/projects", json=body, headers=headers
-    )
+    project_response = rollbar_test_client.create_project()
     project = project_response.json()
-    assert project_response.ok
     project_id = project["result"]["id"]
 
-    # 2) get access token with scope post_server_item, read from project details
-    headers = {
-        "Content-Type": "application/json",
-        "X-Rollbar-Access-Token": rollbar_secrets["read_access_token"],
-        #     Use an Account Access Token with 'read' scope
-    }
-    url = f"{base_url}/api/1/project/{project_id}/access_tokens"
-    access_token_response = requests.get(url=url, headers=headers)
-    assert access_token_response.ok
-    access_tokens = access_token_response.json()
-    access_tokens_result = access_tokens["result"]
-    import pdb
+    error_message = (
+        f"Project with project id [{project_id}] could not be added to Rollbar"
+    )
+    poll_for_existence(
+        _project_exists,
+        (project_id, rollbar_test_client),
+        error_message=error_message,
+    )
 
-    pdb.set_trace()
+    # 2) get access token with scope post_server_item, read from project details
+    project_access_token_response = rollbar_test_client.get_project(
+        project_id=project_id
+    )
+    access_tokens = project_access_token_response.json()
+    access_tokens_result = access_tokens["result"]
     # fetch only specific tokens from token list
     project_tokens = {"read": "", "post_server_item": ""}
     for token in access_tokens_result:
@@ -170,33 +260,16 @@ def rollbar_create_erasure_data(
             project_tokens["post_server_item"] = token["access_token"]
 
     # 3) create item inside above created project
-    # Use an access token with scope post_server_item
-    headers = {
-        "Content-Type": "application/json",
-        "X-Rollbar-Access-Token": project_tokens["post_server_item"],
-    }
-    body = {
-        "data": {
-            "environment": "production",
-            "body": {
-                "message": {
-                    "body": "Request over threshold of 10 seconds",
-                    "route": "home#index",
-                    "time_elapsed": 15.23,
-                },
-                "level": "error",
-            },
-            "person": {
-                "id": "1",
-                "username": "ethyca",
-                "email": rollbar_erasure_identity_email,
-            },
-        }
-    }
-    item_response = requests.post(
-        url=f"{base_url}/api/1/item/", json=body, headers=headers
+    item_response = rollbar_test_client.create_item(
+        project_tokens, rollbar_erasure_identity_email
     )
-
-    assert item_response.ok
     item = item_response.json()["result"]
+    error_message = (
+        f"Item within project id [{project_id}] could not be added to Rollbar"
+    )
+    poll_for_existence(
+        _item_exists,
+        (project_tokens, rollbar_test_client),
+        error_message=error_message,
+    )
     yield item
