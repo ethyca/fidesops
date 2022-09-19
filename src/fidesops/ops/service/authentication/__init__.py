4faf618f94be51a558be9233c0dd4c54688b3a01
Adam Sachs <adam@Adams-MacBook-Pro.local>
2022-09-19 17:14:11 -0400

    Remove query_param auth strategy
    The query_param auth strategy can now easily be implemented as an
    `api_key` auth strategy, which provides the same functionality and more.

--- src/fidesops/ops/service/authentication/__init__.py
+++ src/fidesops/ops/service/authentication/__init__.py
@@ -4,5 +4,4 @@ from fidesops.ops.service.authentication import (
     authentication_strategy_bearer,
     authentication_strategy_oauth2_authorization_code,
     authentication_strategy_oauth2_client_credentials,
-    authentication_strategy_query_param,
 )
