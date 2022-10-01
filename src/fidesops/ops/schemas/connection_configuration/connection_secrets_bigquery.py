b0915c2a8d339f13ef0586864ef1618143b020c4
wiseaidev <business@wiseai.dev>
2022-10-01 13:31:04 +0300

    add schema examples for schemas/connection_configuration/connection_secrets_bigquery.py
    Signed-off-by: wiseaidev <business@wiseai.dev>

--- src/fidesops/ops/schemas/connection_configuration/connection_secrets_bigquery.py
+++ src/fidesops/ops/schemas/connection_configuration/connection_secrets_bigquery.py
@@ -22,6 +22,23 @@ class KeyfileCreds(BaseModel):
     auth_provider_x509_cert_url: Optional[str] = None
     client_x509_cert_url: Optional[str] = None
 
+    class Config:
+        """Add example for each field"""
+        schema_extra = {
+            "example": {
+                "type": "service_account",
+                "project_id": "XXXXXXXXXXXXXXXXXXXX",
+                "private_key_id": "key-id",
+                "private_key": "-----BEGIN PRIVATE KEY-----\nprivate-key\n-----END PRIVATE KEY-----\n",
+                "client_email": "service-account-email",
+                "client_id": "XXXXXXXXXXXXXXXXXXXX",
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://accounts.google.com/o/oauth2/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/service-account-email"
+            }
+        }
+
 
 class BigQuerySchema(ConnectionConfigSecretsSchema):
     """Schema to validate the secrets needed to connect to BigQuery"""
@@ -30,6 +47,15 @@ class BigQuerySchema(ConnectionConfigSecretsSchema):
     keyfile_creds: KeyfileCreds
 
     _required_components: List[str] = ["keyfile_creds"]
+
+    class Config:
+        """Add example for each field"""
+        schema_extra = {
+            "example": {
+                "dataset": None,
+                "keyfile_creds": KeyfileCreds(project_id="XXXXXXXXXXXXXXXXXXXX")
+            }
+        }
 
 
 class BigQueryDocsSchema(BigQuerySchema, NoValidationSchema):
