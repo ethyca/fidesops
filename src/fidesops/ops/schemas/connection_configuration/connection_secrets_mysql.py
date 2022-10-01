b7dca49ff7343aeb5f063020edc326d00dbb5ac1
wiseaidev <business@wiseai.dev>
2022-10-01 13:48:03 +0300

    add schema examples for schemas/connection_configuration/connection_secrets_mysql.py
    Signed-off-by: wiseaidev <business@wiseai.dev>

--- src/fidesops/ops/schemas/connection_configuration/connection_secrets_mysql.py
+++ src/fidesops/ops/schemas/connection_configuration/connection_secrets_mysql.py
@@ -19,6 +19,18 @@ class MySQLSchema(ConnectionConfigSecretsSchema):
 
     _required_components: List[str] = ["host"]
 
+    class Config:
+        """Add example for each field"""
+        schema_extra = {
+            "example": {
+                "username": "Mahmoud",
+                "password": "my_super_duper_secret_password",
+                "dbname": "my_db",
+                "host": "localhost",
+                "port": 3306,
+            }
+        }
+
 
 class MySQLDocsSchema(MySQLSchema, NoValidationSchema):
     """MySQL Secrets Schema for API Docs"""
