7918839cad2fc53580598711d21fda3301d4677a
Eduardo Armendariz <eduardo@ethyca.com>
2022-10-03 09:38:23 -0700

    Update typo in tests

--- tests/ops/integration_tests/limiter/test_rate_limiter.py
+++ tests/ops/integration_tests/limiter/test_rate_limiter.py
@@ -6,15 +6,16 @@ from fidesops.ops.service.connectors.limiter.rate_limiter import RateLimiter, Ra
 def test_limiter_respecsts_rate_limit() -> None:
     """Make a number of calls which requires limiter slow down and verify limit is not breached"""
     limiter: RateLimiter = RateLimiter()
+    num_calls = 500
     call_log = {}
-    for _ in range(500):
+    for _ in range(num_calls):
         limiter.limit(key="my_test_key", rate_limit=100, period=RateLimiterPeriod.SECONDS)
         current_time = int(time.time())
         count = call_log.get(current_time, 0)
         call_log[current_time] = count + 1
         time.sleep(.002)
 
-    assert sum(call_log.values()) == 1500
+    assert sum(call_log.values()) == num_calls
     for value in call_log.values():
         #even though we set the rate limit at 100 there is a small chance our 
         #seconds dont line up with the second used by the rate limiter
